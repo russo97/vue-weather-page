61ffab8ead3a3e17bbaa9450a208f90e6c570e8b
Wanderson Silva <wandersonsilva768@gmail.com>
2023-03-11 02:06:15 -0300

    feat: commit dos arquivos iniciais do projeto

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="app w-full h-full">
+    <div class="app__wrapper"></div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App'
+})
+</script>
+
+<style lang="scss" scoped>
+  .app {
+    background: transparent url("./assets/images/background.webp") center / cover no-repeat;
+  }
+</style>
