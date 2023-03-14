3fc5e66500b9effefde090e7b2678c017d12f596
Wanderson Silva <wandersonsilva768@gmail.com>
2023-03-14 00:32:14 -0300

    feat: adição do segundo cloud-icon com cores vindas das variáveis :root definidas no scss

--- src/components/current-weather/index.vue
+++ src/components/current-weather/index.vue
@@ -1,8 +1,12 @@
 <template>
   <section class="curweather">
-    <div class="curweather__wrapper w-full h-full">
+    <div class="curweather__wrapper w-full h-full relative">
       <LazyPrimaryCloudIcon
-        class="curweather__cloud curweather--cloud--primary"
+        class="curweather__cloud curweather__cloud--primary absolute"
+      />
+
+      <LazySecondaryCloudIcon
+        class="curweather__cloud curweather__cloud--secondary absolute"
       />
     </div>
   </section>
@@ -15,11 +19,16 @@ const LazyPrimaryCloudIcon = defineAsyncComponent(() => import(
   /* webpackChunkName: "lazy-primary-cloud-icon" */ '@/components/icons/primary-cloud/index.vue'
 ))
 
+const LazySecondaryCloudIcon = defineAsyncComponent(() => import(
+  /* webpackChunkName: "lazy-secondary-cloud-icon" */ '@/components/icons/secondary-cloud/index.vue'
+))
+
 export default defineComponent({
   name: 'CurrentWeather',
 
   components: {
-    LazyPrimaryCloudIcon
+    LazyPrimaryCloudIcon,
+    LazySecondaryCloudIcon
   },
 
   async setup () {
@@ -36,12 +45,27 @@ export default defineComponent({
     &__wrapper {
       border-radius: 10px;
       background-blend-mode: luminosity;
-      background: #8E87FA url('@/assets/images/weather-bg.webp') center / cover no-repeat;
+      background: var(--purple1) url('@/assets/images/weather-bg.webp') center / cover no-repeat;
     }
 
     &__cloud {
-      fill: white;
-      width: 27.29%;
+      &--primary {
+        z-index: 2;
+        fill: var(--white);
+        width: 27.4%;
+        top: 0;
+        left: 0;
+        transform: translate(-36.496%, -8.363%);
+      }
+
+      &--secondary {
+        z-index: 1;
+        fill: var(--gray1);
+        width: 22.725%;
+        top: 0;
+        left: 0;
+        transform: translate(2.750%, -22.984%);
+      }
     }
   }
 </style>
